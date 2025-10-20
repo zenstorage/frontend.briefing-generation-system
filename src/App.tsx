5e6f7de4da30cbd12a03496f3715f570e9ba768e
zenstorage <bielhenriquek.k@gmail.com>
2025-10-20 13:14:32 -0300

    up

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,8 @@ import Pricing from "./pages/Pricing";
 // export const API_ENDPOINT = process.env.API_ENDPOINT;
 
 // export const API_ENDPOINT="http://localhost:3000"
-export const API_ENDPOINT = context.env.API_ENDPOINT || "http://206.42.50.24:3000";
+// export const API_ENDPOINT = "http://206.42.50.24:3000";
+export const API_ENDPOINT = "https://ignite-briefs-6nmj11jbl-zenstorages-projects.vercel.app";
 
 const queryClient = new QueryClient();
 
